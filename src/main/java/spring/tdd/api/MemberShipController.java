bf8846913f0c43b20da8960cb3861715ac0b81e8
1101902 <ryool@sk.com>
2022-10-31 20:27:53 +0900

    Feat : MemberShipController 추가

--- src/main/java/spring/tdd/api/MemberShipController.java
+++ src/main/java/spring/tdd/api/MemberShipController.java
@@ -1,6 +1,11 @@
 package spring.tdd.api;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/memberShip")
 @RequiredArgsConstructor
 public class MemberShipController {
+
+    @PostMapping("")
+    public ResponseEntity<MemberShipRequest> registerMemberShip(
+        @RequestHeader(USER_ID_HEADER) final String userId,
+        @RequestBody final MemberShipRequest memberShipRequest
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
