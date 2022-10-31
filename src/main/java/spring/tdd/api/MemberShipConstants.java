7701b884a532cc3875b4cc008919df5c1f0547c5
1101902 <ryool@sk.com>
2022-10-31 20:28:19 +0900

    Feat : Header를 위한 MemberShipConstants class 추가

---
+++ src/main/java/spring/tdd/api/MemberShipConstants.java
@@ -0,0 +1,9 @@
+package spring.tdd.api;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MemberShipConstants {
+    public final static String USER_ID_HEAER = "X-USER-ID";
+}
