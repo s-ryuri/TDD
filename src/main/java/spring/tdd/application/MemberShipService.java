f809ccb0c48f7e8dc47e7babf23e40f7851a2529
1101902 <ryool@sk.com>
2022-10-31 14:07:49 +0900

    Feat : MemberShipService 추가

---
+++ src/main/java/spring/tdd/application/MemberShipService.java
@@ -0,0 +1,13 @@
+package spring.tdd.application;
+
+import org.springframework.stereotype.Service;
+import spring.tdd.domain.MemberShip;
+import spring.tdd.domain.MemberShipType;
+
+@Service
+public class MemberShipService {
+
+    public MemberShip addMemberShip(String userId, MemberShipType memberShipType, int price){
+        return null;
+    }
+}
