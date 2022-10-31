c67564f3938bd61fffae63919047d731f3b5b322
1101902 <ryool@sk.com>
2022-10-31 10:39:42 +0900

    Feat : MemberShipRepository에 Method 추가

--- src/main/java/spring/tdd/infra/MemberShipRepository.java
+++ src/main/java/spring/tdd/infra/MemberShipRepository.java
@@ -2,6 +2,8 @@ package spring.tdd.infra;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import spring.tdd.domain.MemberShip;
+import spring.tdd.domain.MemberShipType;
 
 public interface MemberShipRepository extends JpaRepository<MemberShip,Long> {
+    MemberShip findByUserIdAndMemberShipType(String userId, MemberShipType memberShipType);
 }
