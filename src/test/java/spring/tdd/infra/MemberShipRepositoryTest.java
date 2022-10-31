fd15b8903f79115227a3086f43d07b7bff0e9ec3
1101902 <ryool@sk.com>
2022-10-31 10:20:27 +0900

    Test : 멤버쉽 등록 테스트

--- src/test/java/spring/tdd/infra/MemberShipRepositoryTest.java
+++ src/test/java/spring/tdd/infra/MemberShipRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import spring.tdd.domain.MemberShip;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,5 +18,20 @@ public class MemberShipRepositoryTest {
     @DisplayName("MemberShipRepository가 Bean이 등록되어있는 지 확인")
     void MemberShipRepositoryNotNull() {
         assertThat(memberShipRepository).isNotNull();
+    }
+
+    @Test
+    @DisplayName("멤버십이 제대로 등록되는 지 확인")
+    void 멤버쉽등록() {
+        MemberShip memberShip = MemberShip.builder()
+                                          .name("네이버")
+                                          .point(10000)
+                                          .build();
+
+        MemberShip save = memberShipRepository.save(memberShip);
+
+        assertThat(save.getId()).isNotNull();
+        assertThat(save.getName()).isEqualTo("네이버");
+        assertThat(save.getPoint()).isEqualTo(10000);
     }
 }
