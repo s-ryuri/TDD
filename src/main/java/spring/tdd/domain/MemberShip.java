9e868b85abc66f4cda8d07548e86c1800930263a
1101902 <ryool@sk.com>
2022-10-31 10:20:07 +0900

    Feat : Entity 나머지 생성

--- src/main/java/spring/tdd/domain/MemberShip.java
+++ src/main/java/spring/tdd/domain/MemberShip.java
@@ -1,6 +1,8 @@
 package spring.tdd.domain;
 
 import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
@@ -10,6 +12,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+@Getter
 @Entity
 @Table(name = "member_ship")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -19,4 +22,16 @@ public class MemberShip {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "memberShip_id")
     private Long id;
+
+    @Column(name = "memberShip_name")
+    private String name;
+
+    private int point;
+
+    @Builder
+    public MemberShip(Long id, String name, int point) {
+        this.id = id;
+        this.name = name;
+        this.point = point;
+    }
 }
