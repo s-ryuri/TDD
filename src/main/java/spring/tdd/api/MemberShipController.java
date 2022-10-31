7dd57a1ea1ed4a3a17101e411cefd569abff2144
1101902 <ryool@sk.com>
2022-10-31 16:33:19 +0900

    Test : MemberShipController Test 통과하게 수정

---
+++ src/main/java/spring/tdd/api/MemberShipController.java
@@ -0,0 +1,11 @@
+package spring.tdd.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/memberShip")
+@RequiredArgsConstructor
+public class MemberShipController {
+}
--- src/test/java/spring/tdd/api/MemberShipControllerTest.java
+++ src/test/java/spring/tdd/api/MemberShipControllerTest.java
@@ -4,14 +4,18 @@ import com.google.gson.Gson;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.assertj.core.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MemberShipControllerTest {
 
+    @InjectMocks
     private MemberShipController memberShipController;
 
     private MockMvc mockMvc;
@@ -19,6 +23,8 @@ public class MemberShipControllerTest {
 
     @Test
     void mockMvc가NUll이아님() throws Exception {
+        mockMvc = MockMvcBuilders.standaloneSetup(memberShipController)
+                                 .build();
         assertThat(memberShipController).isNotNull();
         assertThat(mockMvc).isNotNull();
 
