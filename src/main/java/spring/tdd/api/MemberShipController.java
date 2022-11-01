f5e2cd052734fcf07534afdcec1442c2f29a6ec3
s-ryuri <fbfbf1@naver.com>
2022-11-01 15:50:42 +0900

    Feat : MemberShipController Header, PathVariable 추가

--- src/main/java/spring/tdd/api/MemberShipController.java
+++ src/main/java/spring/tdd/api/MemberShipController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -30,6 +31,14 @@ public class MemberShipController {
         @RequestHeader(USER_ID_HEAER) final String userId
     ) {
         return ResponseEntity.ok(memberShipService.getMemberShipList(userId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MemberShipDetailResponse> getMemberShip(@RequestHeader(USER_ID_HEAER) final String userId,
+                                                                  @PathVariable final Long id){
+
+        return ResponseEntity.ok(memberShipService.getMemberShip(id,userId));
+
     }
 
     @PostMapping("")
